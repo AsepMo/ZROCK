5c187210bd77ce0e5fe94e54b713469e91bbb795
AsepMo <asepmo.story@gmail.com>
2020-02-16 06:46:07 +0700

    THIS ApplicationMain

---
+++ app/src/main/java/zrock/application/ApplicationMain.java
@@ -0,0 +1,15 @@
+package zrock.application;
+
+import android.app.Application;
+
+public class ApplicationMain extends Application
+{
+
+	@Override
+	public void onCreate()
+	{
+		// TODO: Implement this method
+		super.onCreate();
+	}
+	
+}
